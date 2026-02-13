beec16a6658255614665b2e0f6fe32c028a58a15
janghyunwoo2 <ehrtk2003+1@gmail.com>
2026-02-13 18:15:27 +0900

    feat: 불필요한 파일 삭제

--- infrastructure/terraform/04-kinesis.tf
+++
@@ -1,7 +0,0 @@
-# Kinesis Data Streams and Firehose
-# 작업: 05_data_pipeline_기본.md (Phase 1)
-# 용도: 실시간 로그 수집 및 S3 전송
-
-# TODO: Kinesis Data Stream (capa-logs-stream)
-# TODO: Kinesis Firehose (capa-logs-firehose)
-# TODO: Firehose Delivery to S3
